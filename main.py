c009d69f04cae6ef9ce0e5e5d6ef0b026429bef1
jade-pond <167834779+jade-pond@users.noreply.github.com>
2024-08-12 22:43:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from dotenv import load_dotenv
-load_dotenv()
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
